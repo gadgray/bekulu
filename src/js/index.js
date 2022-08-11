a748016be28d4aab6b3e6b5baa79d30186c59bb9
gadgray <71590466+gadgray@users.noreply.github.com>
2022-08-12 00:41:53 +0100

    added out heritage menu and oganized the nav am layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
-const btn =  document.getElementById("menu-btn")
-const nav = document.getElementById("menu")
+const btn =  document.getElementById("menu-btn");
+const hBtn = document.querySelector(".h-btn");
+const mobileBtn = document.querySelector(".mobile-btn");
+const mobileHeritage = document.querySelector(".bobile-heritage");
+const heritageBtn = document.querySelector(".heritage");
+const nav = document.getElementById("menu");
 
 btn.addEventListener('click', ()=>{
 
     btn.classList.toggle('open');
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
+})
+
+// higher view ports
+hBtn.addEventListener('click', ()=>{
+    heritageBtn.classList.toggle('hidden')
+    heritageBtn.classList.toggle('flex')
+})
+hBtn.addEventListener('mouseover', ()=>{
+    heritageBtn.classList.toggle('hidden')
+    heritageBtn.classList.toggle('flex')
+})
+
+// moile view
+mobileBtn.addEventListener('click', ()=>{
+    mobileHeritage.classList.toggle('hidden')
+    mobileHeritage.classList.toggle('flex')
+})
+mobileBtn.addEventListener('mouseover', ()=>{
+    mobileHeritage.classList.toggle('hidden')
+    mobileHeritage.classList.toggle('flex')
 })
